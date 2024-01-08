57d82cf722fd9b9e0fb09ca7ff41cb8c7096f9c3
Yevheniia Simaka <yevheniiasimaka@gmail.com>
2024-01-08 13:58:20 -0700

    features and awards done

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-
-const Button = ({ text }) => {
+const Button = () => {
   return (
-    <button className="text-zinc-700 md:px-14 md:py-4 px-8 py-3 m-auto md:m-0 w-fit bg-teal-200 rounded-full hover:bg-transparent duration-200 hover:border-white hover:text-white border-teal-200 border-2">{text}</button>
-  )
-}
+    <div className="text-lg font-semibold cursor-pointer text-zinc-800 md:px-14 md:py-4 px-8 py-3 m-auto md:m-0 w-fit bg-teal-200 rounded-full hover:bg-transparent duration-200 hover:border-white hover:text-white border-teal-200 border-2">
+      Explore
+    </div>
+  );
+};
 
-export default Button
+export default Button;
--- src/components/features.jsx
+++ src/components/features.jsx
@@ -1,28 +1,46 @@
-import { motion } from "framer-motion"
-import { variant2 } from "../utils/motion"
-import { featuredData } from "../assets/dummy"
-import Button from "./button"
+import { motion } from "framer-motion";
+import { variant2 } from "../utils/motion";
+import { featuredData } from "../assets/dummy";
+import Button from "./button";
 
 const Features = () => {
   return (
-    <div className=" min-h-screen p-5 lg:sticky top-28 flex justify-center md:justify-end max-w-[1440px] w-full mx-auto">
-      <div className=" md:max-w-md w-full">
-        <motion.h1 variants={variant2(0.5)}
+    <div className="min-h-screen p-5 pr-16 lg:sticky top-4 pb-10 flex justify-center md:justify-end max=w=[1440px] w-full mx-auto">
+      <div className="md:max-w-md w-full">
+        <motion.h1
+          variants={variant2(0.5)}
+          initial={"hidden"}
+          whileInView={"show"}
+          className="text-teal-200 font-medium text-4xl my-5"
+        >
+          {featuredData.h1}
+        </motion.h1>
+        <motion.div
+          variants={variant2(0.7)}
           initial="hidden"
-          whileInView="show"
-          className=" text-teal-200 font-medium text-4xl my-5">{featuredData.h1}</motion.h1>
-        <motion.div variants={variant2(0.7)}
-          initial="hidden"
-          whileInView="show"
-          className=" bg-slate-900/50 md:bg-slate-700/50 border-2 border-slate-700 p-8 rounded-xl">
-          <video className=" rounded-lg" playsInline loop muted autoPlay preload='true' src={featuredData.video_url}></video>
-          <h1 className="text-4xl text-white font-semibold mt-6">{featuredData.card_h1}</h1>
-          <p className=" text-lg my-5 text-white opacity-40">{featuredData.card_p}</p>
-          <Button text={'Play Now'}/>
+          whileInView={"show"}
+          className="bg-slate-900/50 mg:bg-slate-700/50 border-2 border-slate-700 p-8 rounded-xl"
+        >
+          <video
+            src={featuredData.video_url}
+            className="rounded-lg"
+            playsInline
+            loop
+            muted
+            autoPlay
+            preload="true"
+          ></video>
+          <h1 className="text-4xl text-white font-semibold mt-6">
+            {featuredData.card_h1}
+          </h1>
+          <p className="text-lg my-5 text-white opacity-40">
+            {featuredData.card_p}
+          </p>
+          <Button text={"Play Now"} />
         </motion.div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Features
+export default Features;
